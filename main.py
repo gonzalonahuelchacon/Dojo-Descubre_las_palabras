6aab1524a78d48bc771aa27f6ed5081a85e904f8
Gonzalo <nahuel.chacon19@gmail.com>
2023-10-21 22:12:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-import pygame, sys
+import sys
 from funciones import *
 
 pygame.init()
 
-font = pygame.font.Font(None, 36)
-tiempo = pygame.time.Clock()
+font = pygame.font.Font(None, 50)
+acierto = pygame.mixer.Sound(r"Musica\acierto.mp3")
+error = pygame.mixer.Sound(r"Musica\incorrecto.mp3")
+pygame.mixer.music.load(r"Musica\musica.mp3")
+pygame.mixer.music.play(-1)
 
+running = True
 while running:
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -18,54 +23,56 @@ while running:
                     if texto not in palabras_escritas:
                         palabras_escritas.append(texto)
                         acumulador+=5
-                                       
+                        acierto.play()
+                else:
+                    error.play()                                               
                 texto = ""
             else:
                 if len(texto) < maximo:
-                    if event.unicode in letras_validadas:
+                    if event.unicode in letras_str:
                         texto += event.unicode
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:  # Botón izquierdo del ratón
-                x, y = event.pos
-                if shuffle_button.collidepoint(x, y):
-                    letras = mezclar_palabra(key)
-                    letras_validadas = letras_disponibles(letras)
-                elif clear_button.collidepoint(x, y):
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            x, y = pygame.mouse.get_pos()
+            if shuffle_button.collidepoint(x, y):
+                key = elegir_key(lista_clave)
+                letras = mezclar_palabra(key)
+                letras_a_usar = letras_disponibles(letras)
+                letras_str = str(letras_a_usar)
+            elif clear_button.collidepoint(x, y):
+                texto = ""
+            elif submit_button.collidepoint(x, y):
+                    if texto in lista_palabras[key]:
+                        if texto not in palabras_escritas:
+                            palabras_escritas.append(texto)
+                            acumulador+=5
+                            acierto.play()
+                    else:
+                        error.play()                                       
                     texto = ""
-                elif submit_button.collidepoint(x, y):
-                    if  texto in lista_palabras[key]:
-                        if texto not in palabras_escritas:  # Verifica si la palabra no está en palabras_escritas
-                            palabras_escritas.add(texto)  # Agrega la palabra al conjunto
-                            acumulador += 5  # Aumenta el acumulador en 5
-                    texto = ""
-
-    screen.fill(BLANCO)
-    #Muestra las letras a usar
-    letras_surface = font.render("Letras a usar:" + (letras_str),True,texto_color) 
-    screen.blit(letras_surface,(10,10))
-    #Muestra la palabra ingresada
-    texto_surface = font.render(F"Ingrese una palabra:{texto}", True, texto_color)
-    screen.blit(texto_surface, (10, 50))
-    # Muestra el tiempo restante
-    tiempo_surface = font.render(f"Tiempo restante: {tiempo_restante}", True, texto_color)
-    screen.blit(tiempo_surface, (10, 100))
-    #Muestra el puntaje
-    puntaje = font.render(f"Puntuación:{acumulador}",True,texto_color)
-    screen.blit(puntaje,(10,150))
-    # Muestra las palabras escritas
-    palabras_surface = font.render("Palabras en la lista: " + ', '.join(palabras_escritas), True, texto_color)
-    screen.blit(palabras_surface, (10, 200))
-    shuffle_button, clear_button, submit_button = mostrar_botones(screen)
-    pygame.draw.rect(screen, (0, 0, 0), shuffle_button, 2)  # Dibuja el borde de los botones
-    pygame.draw.rect(screen, (0, 0, 0), clear_button, 2)
-    pygame.draw.rect(screen, (0, 0, 0), submit_button, 2)
-    pygame.display.flip()
-    tiempo_restante -= 1
+                    
+    tiempo_actual = pygame.time.get_ticks()
+    tiempo_transcurrido = tiempo_actual - tiempo_inicial
+    tiempo_restante = max(0,duracion-tiempo_transcurrido)
     if tiempo_restante <= 0:
         running = False
 
-    tiempo.tick(60)
+    segundos = tiempo_restante//1000
+    screen.blit(fondo,(0,0))
+    letras_surface = font.render("Letras a usar:" + (letras_str),True,texto_color) 
+    screen.blit(letras_surface,(500, 400))
+    texto_surface = font.render(F"Ingrese una palabra:{texto}", True, texto_color)
+    screen.blit(texto_surface, (500, 500))
+    tiempo_surface = font.render(f"Tiempo restante: {segundos}", True, texto_color)
+    screen.blit(tiempo_surface, (1250, 10))
+    puntaje = font.render(f"Puntuación:{acumulador}",True,texto_color)
+    screen.blit(puntaje,(10,10))
+    palabras_surface = font.render("Palabras en la lista: " + ', '.join(palabras_escritas), True, texto_color)
+    screen.blit(palabras_surface, (10, 700))
+    shuffle_button, clear_button, submit_button = mostrar_botones(screen)
+    
+    pygame.display.flip()
+    
+    tiempo.tick(FPS)
 print(f"El puntaje obtenido es: {acumulador}")
 pygame.quit()
 sys.exit() 
-
