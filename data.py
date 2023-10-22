8f1876b0a65f8d123dbb8244932569025c556798
Gonzalo <nahuel.chacon19@gmail.com>
2023-10-21 22:12:58 -0300

    Update data.py

--- data.py
+++ data.py
@@ -1,18 +1,23 @@
 lista_palabras = {
     "mares": 
-    [
-        "remas", "resma", "mersa", "mesar", "meras", "mares", "armes", "ares", 
-        "meas", "sera", "reas", "mesa", "rase", "raes", "eras", "sema", "mase", 
-        "arme", "mera", "mear", "rema", "ames", "res", "esa", "sea", "mar", 
-        "ser", "mas", "ras", "mes", "ase", "rae", "era", "rea", "mea", "are", "ame"
-    ],
+        [
+            "remas", "resma", "mersa", "mesar", "meras", "mares", "armes", "ares", 
+            "meas", "sera", "reas", "mesa", "rase", "raes", "eras", "sema", "mase", 
+            "arme", "mera", "mear", "rema", "ames", "res", "esa", "sea", "mar", 
+            "ser", "mas", "ras", "mes", "ase", "rae", "era", "rea", "mea", "are", "ame"
+        ],
     
     "relojeria": 
-    [
-        "reloj", "alejo", "rojo", "jaleo", "lea", "ola", "rola", "real", "ole"
-    ],
+        [
+            "relojeria", "relojera", "relojear", "relajeo", "realojé", "realoje", "realejo", "ojearle", "realero", "relojea", "rojearé", "loreare", "lorearé", "orearle", "relejar", "orejear", "orejera", "erario", "elijar", "roería", "rilaré", "relajo", "relajó", "rilare", "olería", "oliera", "larije", "lijare", "lijaré", "rielar", "reírla", "irreal", "reírlo", "ajorre", "rolare", "rolaré", "reloj"
+        ],
     "milanesa":
-    [
-        "alamines", "almenáis", "amilanes", "animales", "enlamáis", "laminase", "mesalina", "milanesa"
-    ]
+        [
+            "alamines", "almenáis", "amilanes", "animales", "enlamáis", "laminase", "mesalina", "milanesa", "esa", "milan", "aminase", "amanéis", "masiena", "maneáis", "amnesia", "anemias", "animase", "emanáis", "imanase", "malsine", "malsiné", "melinas", "minales", "salamín", "semanal", "salmean", "enlamas", "almenas", "seminal", "milenas", "milanés", "amaines", "limasen", "ánimas", "imanas", "mania", "esa", "mil"
+        ],
+    
+    "floresta": 
+        [
+            "flotarse", "reflotas", "forestal", "flotares", "flotarse", "floresta", "floraste", "estrofa", "saltero", "soletar", "rotales", "rolaste", "resaltó", "resalto", "relatos", "orlaste", "loteras", "lateros", "estarlo", "trasloé", "farseto", "estofar", "flores", "eslora", "loares", "losare", "losaré", "olerás", "solare", "orales", "solaré", "solear", "solerá", "solera", "rol", "flor", "esta"
+        ]
 }
